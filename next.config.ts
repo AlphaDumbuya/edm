58237cb325cec175e667e461bd5aeff322bb1cdc
Alpha Dumbuya <alphadumbuya7@gmail.com>
2025-05-18 20:48:58 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > Unhandled Runtime Error: Error: Invalid src prop (https://plus.unsplash.com/premium_photo-1723759212099-d33000267d6f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8RGlzY2lwbGVzaGlwfGVufDB8fDB8fHww) on `next/image`, hostname "plus.unsplash.com" is not configured under images in your `next.config.js`
    > See more info: https://nextjs.org/docs/messages/next-image-unconfigured-host. Error source: src/app/ministries/evangelism/page.tsx (25:11) @ EvangelismPage
    >
    >   23 |         </div>
    >   24 |         <div className="rounded-lg overflow-hidden shadow-xl h-80 relative">
    > > 25 |           <Image
    >      |           ^
    >   26 |             src="https://plus.unsplash.com/premium_photo-1723759212099-d33000267d6f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8RGlzY2lwbGVzaGlwfGVufDB8fDB8fHww"
    >   27 |             alt="EDM evangelism team sharing the Gospel"
    >   28 |             layout="fill"
    >
    > Call Stack
    > 7
    >
    > Show 5 ignore-listed frame(s)
    > Array.map
    > <anonymous> (0:0)
    > EvangelismPage
    > src/app/ministries/evangelism/page.tsx (25:11)

--- next.config.ts
+++ next.config.ts
@@ -41,6 +41,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'plus.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
